1ed7009d18337eebb8dae6b34a435dbc09b91562
lloppy <ankudinovapol@gmail.com>
2024-09-23 23:48:01 +0500

    done logic in greeting screen

--- app/src/main/java/com/example/asnova/screen/log_in/SignInScreen.kt
+++ app/src/main/java/com/example/asnova/screen/log_in/SignInScreen.kt
@@ -1,12 +1,6 @@
 package com.example.asnova.screen.log_in
 
 import android.util.Log
-import android.widget.Toast
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -28,8 +22,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
@@ -41,6 +36,7 @@ import com.example.asnova.ui.theme.greenAsnova
 import com.vk.id.multibranding.OAuthListWidget
 import com.vk.id.onetap.compose.onetap.sheet.OneTapBottomSheet
 import com.vk.id.onetap.compose.onetap.sheet.rememberOneTapBottomSheetState
+import kotlinx.coroutines.delay
 
 @Composable
 fun SignInScreen(
@@ -49,110 +45,124 @@ fun SignInScreen(
     goProfile: () -> Unit,
     goOtp: () -> Unit
 ) {
-    val context = LocalContext.current
     val bottomSheetState = rememberOneTapBottomSheetState()
-    bottomSheetState.show()
+
+    LaunchedEffect(Unit) {
+        delay(15000)
+        bottomSheetState.show()
+    }
 
     LaunchedEffect(key1 = state.signInError) {
         state.signInError?.let { error ->
-            Toast.makeText(
-                context,
-                error,
-                Toast.LENGTH_LONG
-            ).show()
+            Log.e("login_info", "$error! Вход не выполнен")
+            onSignInClick.invoke()
         }
     }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
-            .padding(16.dp),
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFF282C27),
+                        Color(0xFF181A18),
+                        Color(0xFF1A1C1A),
+                        Color(0xFF1D1F1E),
+                        Color(0xFF18231B)
+                    ),
+                    start = Offset(0f, Float.POSITIVE_INFINITY),
+                    end = Offset(Float.POSITIVE_INFINITY, 0f)
+                )
+            ),
         contentAlignment = Alignment.Center
     ) {
-        AnimatedVisibility(
-            visible = true,
-            enter = slideInVertically(
-                tween(2200, easing = FastOutSlowInEasing),
-                initialOffsetY = { it / 8 }) + fadeIn(tween(2200, easing = FastOutSlowInEasing))
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomStart
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
+            Image(
+                painter = painterResource(id = R.drawable.logo_white_background),
+                contentDescription = null,
+                alignment = Alignment.BottomStart,
+                modifier = Modifier.fillMaxSize(0.9f),
+            )
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // Google Sign-In Button
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .shadow(
+                        elevation = 2.dp,
+                        shape = RoundedCornerShape(percent = 12),
+                        spotColor = Color.Black,
+                        ambientColor = Color.Black
+                    )
+                    .background(Color.White)
+                    .clip(RoundedCornerShape(12))
+                    .clickable(onClick = onSignInClick)
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.asnova_logo),
-                    contentDescription = "logo",
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp)
-                )
-                Spacer(modifier = Modifier.height(32.dp))
-
-                // Google Sign-In Button
-                Box(
+                    painter = painterResource(id = R.drawable.googlelogo),
+                    contentDescription = stringResource(R.string.google_logo),
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp)
-                        .shadow(
-                            elevation = 2.dp,
-                            shape = RoundedCornerShape(percent = 12),
-                            spotColor = Color.Black,
-                            ambientColor = Color.Black
-                        )
-                        .background(Color.White)
-                        .clip(RoundedCornerShape(12))
-                        .clickable(onClick = onSignInClick)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.googlelogo),
-                        contentDescription = stringResource(R.string.google_logo),
-                        modifier = Modifier
-                            .padding(start = 8.dp)
-                            .size(48.dp)
-                            .align(Alignment.CenterStart)
-                    )
-                    Text(
-                        text = stringResource(R.string.google_sign_in),
-                        fontFamily = FontFamily(Font(R.font.pretendbold)),
-                        color = Color(0xFF1F1F1F),
-                        fontSize = 16.sp,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-
-                // VK Sign-In Button
-
-                // не работает, потому что нужно выложить приложение в плей маркет
-                // doesnt work - info here: https://id.vk.com/about/business/go/docs/ru/vkid/latest/oauth/oauth-mail/configure/application-settings#Obshie-nastrojki
-
-                OAuthListWidget(
-                    onFail = { oAuth, fail ->
-                        Log.e("AuthFail", "Error: ${fail.description}")
-                    },
-                    onAuth = { oAuth, token ->
-                        Log.d("Auth", "Token name: ${token.userData.email}")
-                    }
+                        .padding(start = 8.dp)
+                        .size(48.dp)
+                        .align(Alignment.CenterStart)
                 )
-                Spacer(modifier = Modifier.height(16.dp))
-
-                // https://id.vk.com/about/business/go/docs/ru/vkid/latest/vk-id/connection/android/floating-onetap
-                OneTapBottomSheet(
-                    state = bottomSheetState,
-                    onAuth = { oAuth, token ->
-                        Log.d("Auth", "Token name: ${token.userData.email}")
-                    },
-                    serviceName = stringResource(R.string.service_name)
+                Text(
+                    text = stringResource(R.string.google_sign_in),
+                    fontFamily = FontFamily(Font(R.font.pretendbold)),
+                    color = Color(0xFF1F1F1F),
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.Center)
                 )
-                Spacer(modifier = Modifier.height(16.dp))
-
-//                TextButton(onClick = { goOtp.invoke() }) {
-//                    Text(
-//                        text = stringResource(R.string.login_using_phone),
-//                        color = greenAsnova,
-//                        fontSize = 12.sp
-//                    )
-//                }
-//                Spacer(modifier = Modifier.height(16.dp))
             }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // VK Sign-In Button
+
+            // не работает, потому что нужно выложить приложение в плей маркет
+            // doesnt work - info here: https://id.vk.com/about/business/go/docs/ru/vkid/latest/oauth/oauth-mail/configure/application-settings#Obshie-nastrojki
+
+            OAuthListWidget(
+                onFail = { oAuth, fail ->
+                    Log.e("AuthFail", "Error: ${fail.description}")
+                },
+                onAuth = { oAuth, token ->
+                    Log.d("Auth", "Token name: ${token.userData.email}")
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // https://id.vk.com/about/business/go/docs/ru/vkid/latest/vk-id/connection/android/floating-onetap
+            OneTapBottomSheet(
+                state = bottomSheetState,
+                onAuth = { oAuth, token ->
+                    Log.d("Auth", "Token name: ${token.userData.email}")
+                },
+                serviceName = stringResource(R.string.service_name)
+            )
+
+//            Spacer(modifier = Modifier.height(40.dp))
+//
+//            TextButton(onClick = { goOtp.invoke() }) {
+//                Text(
+//                    text = stringResource(R.string.login_using_phone),
+//                    color = greenAsnova,
+//                    fontSize = 12.sp
+//                )
+//            }
+//            Spacer(modifier = Modifier.height(16.dp))
+
         }
     }
 }
