f583613af613cd01bf789791b56cca58bc868677
lloppy <ankudinovapol@gmail.com>
2024-10-02 17:06:22 +0500

    start to add FirebaseMessagingService

---
+++ core/domain/src/main/java/com/asnova/domain/usecase/AddNewClassUseCase.kt
@@ -0,0 +1,10 @@
+package com.asnova.domain.usecase
+
+import com.asnova.domain.repository.firebase.ScheduleRepository
+import com.asnova.model.Resource
+import com.asnova.model.ScheduleFirebase
+
+class AddNewClassUseCase(
+) {
+
+}
